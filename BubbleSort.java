db60a5073bb34a64d121abe0a80e0b213b7a6839
mRaines08 <mallory.raines@stu.daviess.kyschools.us>
2025-04-18 13:29:05 +0000

    homeworkCorrections41825

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,20 +6,16 @@ public class BubbleSort
 	public static void main(String[] args)
   {
     int[] myNums = {12, 15, 0, 44, 13, 1, 2};
-		int passes;
-		int comparisons;
-		int tempNum;
+		
+	System.out.println(Arrays.toString(myNums));
 
-		for(int i = myNums.length - 1; i >= 0; i--)
-			for(int num = 0; num < i; num++)
+	for(int pass = myNums.length - 1; pass > 0; pass--)
+		for(int num = 0; num < pass; num++)
+			if(myNums[num] > myNums[num + 1])
 			{
 				int temp = myNums[num];
-
-				if(myNums[num] > myNums[num + 1])
-				{
-					myNums[num] = myNums[num + 1];
-					myNums[num + 1] = temp;
-				}
+				myNums[num] = myNums[num + 1];
+				myNums[num + 1] = temp;
 			}
 
 		System.out.println(Arrays.toString(myNums));
